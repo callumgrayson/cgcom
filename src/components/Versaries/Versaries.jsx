7a440d54b88c91b1391f6874ab54bef0a59b72bd
Callum Grayson <cg@CMac.local>
2024-07-21 09:05:51 +1200

    Adds phi values

--- src/components/Versaries/Versaries.jsx
+++ src/components/Versaries/Versaries.jsx
@@ -4,7 +4,64 @@ import InputDateTime from "./InputDateTime";
 import InputTimezone from "./InputTimezone";
 import "./Versaries.css";
 
+function solveEmoment() {
+  // console.log("solve E moment");
+  // if (false) {
+  //   console.log("baseObj", baseObj);
+  //   console.log("inDateString", inDateString);
+  //   console.log("baseObj", baseObj);
+  //   console.log("momentBase", momentBase);
+  //   console.log("momentBase.unix()", momentBase.unix());
+  //   console.log("momentBase.format(formatFull)", momentBase.format(formatFull));
+  //   const valueEyears = 85780560807;
+  //   const zBirthEpoch = new Date("2021-02-12").valueOf(); //
+  //   console.log("zBirthEpoch", zBirthEpoch);
+  //   const zeEpoch = zBirthEpoch + valueEyears;
+  //   console.log("zeEpoch", zeEpoch);
+  //   const isoZeEpoch = new Date(zeEpoch);
+  //   console.log("isoZeEpoch", isoZeEpoch);
+  // }
+  const formatShort = "YYYY MMM DD";
+  const inString = "2020-01-01T00:00:00.001Z";
+  const startM = moment(inString).toISOString();
+  const startN = new Date(inString).toISOString();
+  console.log("startM, startN", startM, startN);
+
+  const eYears = Math.E;
+  const convEYearsMs = eYears * 365.2425 * 24 * 60 * 60 * 1000;
+  const valueMsOfEyears = 85780560807;
+  console.log(
+    "convEYearsMs, valueMsOfEyears",
+    convEYearsMs,
+    valueMsOfEyears,
+    convEYearsMs - valueMsOfEyears
+  );
+
+  const momAdded = moment(inString).add(valueMsOfEyears, "milliseconds");
+  console.log("momAdded", momAdded);
+  const momAddedYears = moment(inString).add(Math.E, "years");
+  console.log("momAddedYears", momAddedYears);
+  const years2 = moment(inString).add(2, "years");
+  console.log("years2", years2);
+  const yearsPoint71828 = moment(inString).add(Math.E - 2, "years");
+  console.log("yearsPoint71828", yearsPoint71828);
+  const conv_2YearsMs = (Math.E - 2) * 365.2425 * 24 * 60 * 60 * 1000;
+  const yearsp71828ms = moment(inString).add(conv_2YearsMs, "milliseconds");
+  console.log("yearsp71828ms", yearsp71828ms);
+  const yearSplit1 = moment(inString).add(0.5, "years").add(0.5, "years");
+  console.log("yearSplit1", yearSplit1);
+  console.log("Math.E", Math.E);
+  const momAddedMonths = moment(inString).add(Math.E * 12, "months");
+  console.log("momAddedMonths", momAddedMonths);
+  console.log("momAddedYears", momAddedYears);
+}
+
+// solveEmoment();
+
+const PHI = (Math.sqrt(5) + 1) / 2;
+
 const base10sMap = [
+  { unit: "seconds", value: PHI },
   { unit: "seconds", value: Math.E },
   { unit: "seconds", value: Math.PI },
   { unit: "seconds", value: 10000 },
@@ -14,6 +71,7 @@ const base10sMap = [
   { unit: "seconds", value: 100000000 },
   { unit: "seconds", value: 1000000000 },
 
+  { unit: "minutes", value: PHI },
   { unit: "minutes", value: Math.E },
   { unit: "minutes", value: Math.PI },
   { unit: "minutes", value: 1000 },
@@ -22,6 +80,7 @@ const base10sMap = [
   { unit: "minutes", value: 1000000 },
   { unit: "minutes", value: 10000000 },
 
+  { unit: "hours", value: PHI },
   { unit: "hours", value: Math.E },
   { unit: "hours", value: Math.PI },
   { unit: "hours", value: 100 },
@@ -30,6 +89,7 @@ const base10sMap = [
   { unit: "hours", value: 100000 },
   { unit: "hours", value: 1000000 },
 
+  { unit: "days", value: PHI },
   { unit: "days", value: Math.E },
   { unit: "days", value: Math.PI },
   { unit: "days", value: 1 },
@@ -38,6 +98,7 @@ const base10sMap = [
   { unit: "days", value: 1000 },
   { unit: "days", value: 10000 },
 
+  { unit: "weeks", value: PHI },
   { unit: "weeks", value: Math.E },
   { unit: "weeks", value: Math.PI },
   { unit: "weeks", value: 1 },
@@ -45,12 +106,14 @@ const base10sMap = [
   { unit: "weeks", value: 100 },
   { unit: "weeks", value: 1000 },
 
+  { unit: "months", value: PHI },
   { unit: "months", value: Math.E },
   { unit: "months", value: Math.PI },
   { unit: "months", value: 10 },
   { unit: "months", value: 100 },
   { unit: "months", value: 1000 },
 
+  { unit: "years", value: PHI },
   { unit: "years", value: Math.E },
   { unit: "years", value: Math.PI },
 ];
@@ -67,6 +130,14 @@ const userTzGuess = moment.tz.guess();
 const getDisplayDates = (inDateString) => {
   const momentBirth = moment(inDateString);
   const baseArray = base10sMap.map((baseObj) => {
+    // TODO - use ms instead of years.
+    // if (
+    //   baseObj.value === Math.E ||
+    //   baseObj.value === Math.PI ||
+    //   baseObj.value === PHI
+    // ) {
+    //   console.log("baseObj", baseObj);
+    // }
     const momentBase = moment(inDateString).add(baseObj.value, baseObj.unit);
 
     return {
